84c5ed37d7705e58fc4db101566417446457093b
JosefGio <108878524+JosefGio@users.noreply.github.com>
2023-06-07 09:56:03 -0300

    Add files via upload

--- control.py
+++ control.py
@@ -7,6 +7,7 @@ def getKey (keyName):
     ans = False
     for eve in pygame.event.get():pass
     keyInput = pygame.key.get_pressed()
+    
     myKey = getattr(pygame, 'K_{}'. format(keyName))
     if keyInput [myKey]:
         ans = True
@@ -14,50 +15,50 @@ def getKey (keyName):
     return ans
     
 def main():
-    if getKey ('w'):
-       # print ('Pra cima')
+    if getKey ('KP8'):
+        print ('Norte')
         #MOTOR 1
-        p0.value(1)
-        p1.value(0)
+       # p0.value(1)
+       # p1.value(0)
         #MOTOR 2
-        p2.value(0)
-        p3.value(1)
+       # p2.value(0)
+       #  p3.value(1)
         #MOTOR 3
-        p4.value(1)
-        p5.value(0)
+       #  p4.value(1)
+       # p5.value(0)
         #MOTOR 4
-        p6.value(0)
-        p7.value(1)
-    if getKey ('s'):
-       # print ('Pra baixo')
+       # p6.value(0)
+       # p7.value(1)
+    if getKey ('KP2'):
+        print ('Sul')
          #MOTOR 1
-        p0.value(0)
-        p1.value(1)
+       # p0.value(0)
+       # p1.value(1)
         #MOTOR 2
-        p2.value(1)
-        p3.value(0)
+       # p2.value(1)
+       # p3.value(0)
         #MOTOR 3
-        p4.value(0)
-        p5.value(1)
+       # p4.value(0)
+       # p5.value(1)
         #MOTOR 4
-        p6.value(1)
-        p7.value(0)
-    if getKey ('d'):
-       # print ('Para direita')
+       # p6.value(1)
+       # p7.value(0)
+    if getKey ('KP6'):
+        print ('Leste')
         #MOTOR 1
-        p0.value(1)
-        p1.value(0)
+       # p0.value(1)
+       # p1.value(0)
         #MOTOR 2
-        p2.value(1)
-        p3.value(0)
+       # p2.value(1)
+       # p3.value(0)
         #MOTOR 3
-        p4.value(0)
-        p5.value(1)
+      #  p4.value(0)
+       # p5.value(1)
         #MOTOR 4
-        p6.value(0)
-        p7.value(1)
-    if getKey ('a'):
-       # print ('Para a esquerda')
+      #  p6.value(0)
+      # p7.value(1)
+    if getKey ('KP4'):
+        print ('Oeste')
     
 if __name__ == '__main__':
     init()
